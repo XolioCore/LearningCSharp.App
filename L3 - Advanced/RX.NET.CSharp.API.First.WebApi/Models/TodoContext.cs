4187c352efeee3c9fbd294a09b006c964bd4d80a
rafaelxoliolab <63336526+rafaelxoliolab@users.noreply.github.com>
2024-08-04 22:31:56 -0600

    adding Web APIs example

---
+++ L3 - Advanced/RX.NET.CSharp.API.First.WebApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuGet.Configuration;
+
+namespace RX.NET.CSharp.API.First.WebApi.Models
+{
+    public class TodoContext : DbContext {
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
